5ef2891403337202cbadb2ab1feaad329e605b51
Dynasty511 <jukang@s.sfusd.edu>
2023-02-03 15:24:13 -0800

    Corrected spawning of dogs and balls

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -1,30 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutOfBounds : MonoBehaviour
+public class DestroyOutOfBoundsX : MonoBehaviour
 {
     private float leftLimit = -30;
     private float bottomLimit = -5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        // If an object goes past the players view in the game, remove that object
+        // Destroy dogs if x position less than left limit
         if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
-            Debug.Log("Game Over!");
         } 
-
+        // Destroy balls if y position is less than bottomLimit
         else if (transform.position.y < bottomLimit)
         {
             Destroy(gameObject);
         }
+
     }
 }
